b2a6584b4346ba5f292faf8c04e930b36f607d01
Abrar Zaman <abrarzaman2003@gmail.com>
2021-02-22 11:26:43 -0600

    added the option to change the number of boxes, and fixed the dragging issue

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
 
 
 const container = document.querySelector("#container");
-const numBox = 10;
+console.log("helo");
+
+var numBox = 10;
 
 
 
 
 
-const contStyle = getComputedStyle(container);
-const contHeight = parseInt(contStyle.height);
-const contWidth= contStyle.width;
 
 
-const pixelSize  = (contHeight / numBox);
+
+
+
 
 var mouseDown = 0;
 document.body.onmousedown = function(){
@@ -24,42 +25,57 @@ document.body.onmouseup = function(){
     mouseDown = 0;
 }
 
+function generateGrid(container, numBox){
+    const contStyle = getComputedStyle(container);
+    const contHeight = parseInt(contStyle.height);
+    const contWidth= contStyle.width;
 
 
-for (i = 0; i<numBox; i++){
-    for (j = 0; j<numBox; j++){
-        
-        let pixel = document.createElement('div');
-        const pixelID = ((i*numBox)+j)
-
-        pixel.setAttribute('class','pixel');
-        pixel.setAttribute('id' , pixelID );
-        pixel.style.height = pixelSize+"px";
-        pixel.style.width=pixelSize+"px";
-        
-        
-        
-        
-        container.appendChild(pixel);
-        let pixelStyle = getComputedStyle(pixel);
-        pixel.addEventListener('mousemove', function (e){
-            if (mouseDown == 1){
-                const x = getComputedStyle(e.target);
-            console.log(x.backgroundColor == 'rgb(0, 128, 0)');
-            if(x.backgroundColor == 'rgb(0, 128, 0)'){
-                e.target.style.background = 'blue'
-            }/*else{
-                e.target.style.background = 'green';
-            }
-            */
-            }
+    const pixelSize  = (contHeight / numBox);
+    
+    
+    for (i = 0; i<numBox; i++){
+        for (j = 0; j<numBox; j++){
             
-
-
+            let pixel = document.createElement('div');
             
-        });
+    
+            pixel.setAttribute('class','pixel');
+            pixel.setAttribute('ondragstart', "return false");
+            pixel.style.height = pixelSize+"px";
+            pixel.style.width=pixelSize+"px";
+            
+            
+            
+            
+            container.appendChild(pixel);
+            let pixelStyle = getComputedStyle(pixel);
+            pixel.addEventListener('mousemove', function (e){
+                if (mouseDown == 1){
+                    const x = getComputedStyle(e.target);
+                if(x.backgroundColor == 'rgb(0, 128, 0)'){
+                    e.target.style.background = 'blue'
+                }/*else{
+                    e.target.style.background = 'green';
+                }
+                */
+                }           
+            });
+        }
     }
 }
+
+generateGrid(container,numBox);
+
+const but = document.getElementById("but");
+but.addEventListener('click', function(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    generateGrid(container, document.getElementById("in").value);
+
+})
+
 
 
 
